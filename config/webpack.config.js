2fdf96b5f6a098970eadad7b0f89d21209b0f28f
Jacques Matthieu <matthieu.jacques@link-value.fr>
2017-07-28 22:30:26 +0200

    odify webpack for fileloader & stylelinter

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,45 +1,83 @@
-const   path = require('path'),
-        root = path.resolve(__dirname),
-        ExtractTextPlugin = require("extract-text-webpack-plugin"),
-        webpack = require('webpack')
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const StyleLintPlugin = require('stylelint-webpack-plugin');
+
+const TextPlugin = new ExtractTextPlugin({
+  filename: '../css/main.css',
+  disable: false,
+  allChunks: true}
+);
+
+const StyleLint = new StyleLintPlugin({
+  configFile: '.stylelintrc',
+  quiet: true, // Use webpack performances hints entry instead
+  syntax: 'scss',
+  failOnError: false,
+  emitErrors: false
+});
 
 module.exports = {
-    entry: {
-        app: ['../app/assets/js/main.js']
-    },
+  entry: {
+    app: ['../app/assets/js/main.js']
+  },
 
-    devtool: 'source-map',
+  devtool: 'source-map',
 
-    output: {
-        path: path.resolve(__dirname, '../dist/assets/js'),
-        filename: 'bundle.js'
-    },
+  output: {
+    path: path.resolve(__dirname, '../dist/assets/js'),
+    filename: 'bundle.js'
+  },
 
-    module: {
-        rules: [
+  module: {
+    rules: [
+      {
+        enforce: 'pre',
+        test: /\.js$/,
+        use: {
+          loader: 'eslint-loader',
+          options: {
+            configFile: '.eslintrc',
+            failOnWarning: false,
+            failOnError: true,
+            emitError: false,
+            emitWarning: false
+          }
+        },
+        exclude: /node_modules/
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.jpe?g$|\.svg$|\.png$/i,
+        use: 'file-loader?name=[name].[ext]'
+      },
+      {
+        test: /\.eot|ttf|woff|woff2$/,
+        use: 'file-loader?name=[name].[ext]'
+      },
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
             {
-                test: /\.js$/,
-                exclude: /(node_modules|bower_components)/,
-                include: root,
-                loader: 'eslint'
-            },{
-                test: /\.scss$/,
-                use: ExtractTextPlugin.extract({
-                    fallback: 'style-loader',
-                    use: ['css-loader', 'sass-loader']
-                })
+              loader: 'css-loader',
+              options: {
+                sourceMap: true
+              }
+            }, {
+              loader: 'sass-loader',
+              options: {
+                sourceMap: true
+              }
             }
-        ]
-    },
-
-    devServer: {
-        hot: true,
-        compress: true,
-        port: 80,
-        stats: {chunks: false}
-    },
-
-    plugins: [
-        new ExtractTextPlugin({filename: '../css/main.css', disable: false, allChunks: true}),
+          ]
+        })
+      }
     ]
-}
+  },
+  plugins: [TextPlugin, StyleLint]
+};
